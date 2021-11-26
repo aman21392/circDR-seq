5e8ffd845d74be4c081a28d7260839cd690668b9
aman21392 <52740993+aman21392@users.noreply.github.com>
2021-11-26 15:13:57 +0530

    Create backsplice.sh

---
+++ backsplice.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+target= path_of_linear_target_fasta_file
+echo "target: "$target
+awk '{if(NR==1) {print $0} else {if($0 ~ /^>/) {print "\n"$0} else {printf $0}}}' $target |awk -v OFS="\n" '/^>/ {getline seq}{L=length(seq); L2=int(L/2); print $0, substr(seq,L2+1) substr(seq,1,L2) }' >$target.backsplice.fa
