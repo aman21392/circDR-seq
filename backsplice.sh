5aa3dfecc2f4f3f5b29c096582f68ea721af3542
aman21392 <52740993+aman21392@users.noreply.github.com>
2021-12-02 16:33:17 +0530

    Update backsplice.sh

--- backsplice.sh
+++ backsplice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ################## convert of linear fasta sequence into backsplice fasta sequence #################################
-target= path_of_linear_target_fasta_file
+target= $1
 echo "target: "$target
 awk '{if(NR==1) {print $0} else {if($0 ~ /^>/) {print "\n"$0} else {printf $0}}}' $target |awk -v OFS="\n" '/^>/ {getline seq}{L=length(seq); L2=int(L/2); print $0, substr(seq,L2+1) substr(seq,1,L2) }' >$target.backsplice.fa
 
