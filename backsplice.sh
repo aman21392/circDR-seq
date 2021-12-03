8db49e146ad98513c45b80710a1730b35d73148a
aman21392 <52740993+aman21392@users.noreply.github.com>
2021-12-03 19:35:34 +0530

    Update backsplice.sh

--- backsplice.sh
+++ backsplice.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-################## convert of linear fasta sequence into backsplice fasta sequence #################################
-target= $1
-echo "target: "$target
-awk '{if(NR==1) {print $0} else {if($0 ~ /^>/) {print "\n"$0} else {printf $0}}}' $target |awk -v OFS="\n" '/^>/ {getline seq}{L=length(seq); L2=int(L/2); print $0, substr(seq,L2+1) substr(seq,1,L2) }' >$target.backsplice.fa
 
+################## convert of linear fasta sequence into backsplice fasta sequence #################################
+target=$1
+echo "target: "$target
+
+awk '{if(NR==1) {print $0} else {if($0 ~ /^>/) {print "\n"$0} else {printf $0}}}' $target |awk -v OFS="\n" '/^>/ {getline seq}{L=length(seq); L2=int(L/2); print $0, substr(seq,L2+1) substr(seq,1,L2) }' >$target.backsplice.fa
 #################### shorten the backsplice target sequence to 100bp library ########################################
 awk -v OFS="\n" '/^>/ {getline seq}{print $0,substr(seq,length(seq)/2-50,100)}' $target.backsplice.fa >$target.backsplice.100bp.fa
