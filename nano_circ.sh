1819434023f9e9d7f799b2ae3e5c83a56c79479f
aman21392 <52740993+aman21392@users.noreply.github.com>
2021-12-08 11:42:49 +0530

    Update nano_circ.sh

--- nano_circ.sh
+++ nano_circ.sh
@@ -9,10 +9,10 @@ pblat -threads=70 -minScore=60 -minIdentity=80 $target $query $query.psl
 ########### use awk command to get only those sequence which span the backplice junction of 20bp upstream and 20bp downstream #############################
 awk '$16<=30 && $17>=70' $query.psl >$query.overlap.psl
 
-############### use of awk command to remove those sequences which have >0.2 mismatches in the alignment ##############################
+############### use of awk command to remove those sequences which have >0.1 mismatches in the alignment ##############################
 cat $query.overlap.psl |awk '$2/($1+$2)<0.1{print $0}' >$query.overlap.mismatch.psl
 
-############### use of awk command to remove those sequences which have >20 gaps in qury and >10 gaps in target sequences for removal of false positive #####################
+############### use of awk command to remove those sequences which have >20 gaps in qury and >20 gaps in target sequences for removal of false positive #####################
 awk '$6<20 && $8<20' $query.overlap.mismatch.psl > $query.overlap.mismatch.gap.psl
 
 #####################extract unique reads ##################################
