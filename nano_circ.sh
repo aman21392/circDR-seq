40faaecc7ed82239aeea244f19532b6a780c4ab4
aman21392 <52740993+aman21392@users.noreply.github.com>
2021-12-25 17:13:49 +0530

    Update nano_circ.sh

--- nano_circ.sh
+++ nano_circ.sh
@@ -18,14 +18,23 @@ cat $query.overlap.psl |awk '$2/($1+$2)<0.1{print $0}' >$query.overlap.mismatch.
 ############### use of awk command to remove those sequences which have >20 gaps in qury and >20 gaps in target sequences for removal of false positive #####################
 awk '$6<20 && $8<20' $query.overlap.mismatch.psl > $query.overlap.mismatch.gap.psl
 
+############################## use awk to sum the 19th column and make new file ##################################
+awk '{print $19}' $query.overlap.mismatch.gap.psl |awk -F , '{ sum = 0; for(i=1; i<=NF; i++) sum += $i; print  sum }' >$query.overlap.mismatch.gap.block.psl
+
+############################# paste the block.psl file into the gap.psl file to make the 22 column psl file #####################################
+paste $query.overlap.mismatch.gap.psl $query.overlap.mismatch.gap.block.psl >$query.overlap.mismatch.gap.block.list.psl
+
+############################ use awk to remove reads on the basis od 22nd column ##########################################
+awk 'NR==FNR{ max[$10]=(max[$10]>$NF? max[$10]:$NF); next }max[$10]==$NF' $query.overlap.mismatch.gap.block.list.psl $query.overlap.mismatch.gap.block.list.psl >$query.overlap.mismatch.gap.block.list.list.psl
+
 #####################extract unique reads ##################################
-awk '{print $10}' $query.overlap.mismatch.gap.psl|sort |uniq -c |awk '$1<=1{print $2}' >read.txt
- 
+awk '{print $10}' $query.overlap.mismatch.gap.block.list.list.psl|sort |uniq -c |awk '$1<=1{print $2}' >read.txt
+
 ######################### make a final file from those unique reads ###################################
-grep -f read.txt $query.overlap.mismatch.gap.psl >output.psl
+grep -f read.txt $query.overlap.mismatch.gap.block.list.list.psl >output.psl
 
 ################### extract the target column and know about the count of circRNA #####################################
 awk '{print $14}'  output.psl|sed 's/|/\t/g' | awk '{print $1}' |sort |uniq -c >$query.count.txt
 
 ################# Delete Temp files ########################
-rm $query.psl $query.overlap.psl $query.overlap.mismatch.psl read.txt $query.overlap.mismatch.gap.psl
+rm $query.psl $query.overlap.psl $query.overlap.mismatch.psl read.txt $query.overlap.mismatch.gap.psl $query.overlap.mismatch.gap.block.psl $query.overlap.mismatch.gap.block.list.psl $query.overlap.mismatch.gap.block.list.list.psl read.txt 
